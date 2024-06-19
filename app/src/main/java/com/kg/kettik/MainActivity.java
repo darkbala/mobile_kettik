6bd20df5bf2a8d38ddad33536a9bbc7ca94029f0
darkbala <rustam.samsaev@gmail.com>
2024-06-19 10:33:32 +0600

    yoyoyo

---
+++ app/src/main/java/com/kg/kettik/MainActivity.java
@@ -0,0 +1,31 @@
+package com.kg.kettik;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_tours, R.id.navigation_contacts, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
